40cf329126ce9d76c3ac3bf2dd77a2f360407795
Ryohei Yamamoto <im1197531@gmail.com>
2021-11-09 09:53:35 +0900

    ログアウト機能追加に対する指摘対応

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import  include,path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,4 +126,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'http://localhost:8000/twitter/login/'
+LOGIN_URL = 'twitter:login'
